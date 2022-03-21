62a3804d31f335f33408a7fb3a270a84b49f5fad
Cid-Jie <pcidjie@gmail.com>
2022-03-21 21:21:49 +0100

    add page with secure form

---
+++ public/form.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulaire de contact</title>
+</head>
+<body>
+    <h1> CONTACT </h1>
+    <p> Do you want to contact us ? </br>
+    Please complete the form below. We will answer you as soon as possible!</p>
+    
+    <form action="thanks.php" method="post">
+        
+        <div>
+            <label for="name">Name&nbsp; :</label>
+            <input type="text" id="name" name="user_name">
+        </div>
+        <div>
+            <label for="mail">E-mail&nbsp; :</label>
+            <input type="email" id="email" name="user_mail">
+        </div>
+        <div>
+            <label for="phone">Phone number : </label>
+            <input type="tel" id="phone" name="user_phone">
+        </div>
+        <div>
+            <label for="subject"> Choose a subject : </label>
+            <select name="about" id="about">
+                <option value="identification">Identification</option>
+                <option value="password">Password</option>
+                <option value="change">Change mail</option>
+                <option value="paiement">Paiement</option>
+                <option value="another">Another</option>
+        </div>
+        <div>
+            <label for="msg">Message : </label>
+            <textarea id="msg" name="user_message" required></textarea>
+        </div>
+        <div class="button">
+            <button type="submit">Send your message</button>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ public/thanks.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+  foreach ($_POST as $input => $value)
+  {
+    if (empty($value))
+    {
+      exit('Please fill in all fields');
+    }
+  }
+
+  if (!filter_var($_POST['user_mail'], FILTER_VALIDATE_EMAIL))
+    {
+      exit('Please enter a valid email address');
+    }    
+
+  if (!filter_var($_POST['user_phone'], FILTER_SANITIZE_NUMBER_INT))
+    {
+      exit('Please enter a valid phone number');
+    } 
+
+?>
+<div class="message">
+    <p>Thank you  <?php echo "{$_POST['user_name']}"; ?> for contact us about <?php echo "{$_POST['about']}"; ?>.
+    </p>
+    <p>One of our advisers will contact you either at this mail : <?php echo "{$_POST['user_mail']}"; ?>or by phone at : <?php echo "{$_POST['user_phone']}"; ?> as soon as possible to process your request : 
+    </p>
+    <?php echo "{$_POST['user_message']}"; ?>
+    </p>
+</div>
+</body>
+</html>
+<?php
+
+
+
+
